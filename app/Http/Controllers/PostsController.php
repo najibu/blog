9974eaad43ace6ad7c1ff4c8a0a06bc02e7a6088
najibu <ns.najibu@gmail.com>
2017-08-19 17:40:09 +0100

    form request data and csrf

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Post;
+
 class PostsController extends Controller
 {
     public function index()
@@ -12,5 +14,22 @@ class PostsController extends Controller
     public function show()
     {
         return view('posts.show');
+    }
+
+    public function create()
+    {
+        return view('posts.create');
+    }
+
+    public function store()
+    {
+        // Create a new post using the request data
+
+        // // save it to the database
+
+        Post::create(request(['title', 'body']));
+
+        // redirect
+        return redirect('/');
     }
 }
---
+++ resources/views/posts/create.blade.php
@@ -0,0 +1,23 @@
+@extends ('layout.master')
+
+@section('content')
+<div class="col-sm-8 blog-main">
+    <h1>Publish a Post</h1>
+
+    <hr>
+
+    <form method="POST" action="/posts">
+        {{ csrf_field() }}
+        <div class="form-group">
+            <label for="title">Title:</label>
+            <input type="text" class="form-control" id="title" name="title">
+        </div>
+        <div class="form-group">
+            <label for="body">Body:</label>
+            <textarea class="form-control" id="body" cols="30" rows="10" name="body"></textarea>
+        </div>
+
+        <button type="submit" class="btn btn-primary">Publish</button>
+</form>
+</div>
+@endsection
