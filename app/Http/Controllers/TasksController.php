d74eabc84fcd4c8fe09b36b70a7fd15e28ca1b50
najibu <ns.najibu@gmail.com>
2017-08-19 16:04:24 +0100

    route model binding

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -14,10 +14,12 @@ class TasksController extends Controller
         return view('tasks.index', compact('tasks'));
     }
 
-    public function show($id)
+    public function show(Task $task)
     {
         //$task = DB::table('tasks')->find($id);
-        $task = Task::find($id);
+        // $task = Task::find($id);
+
+        // return $task;
         return view('tasks.show', compact('task'));
     }
 }
