92b24d40c4b9987cee395527a9a155fe23a26c12
najibu <ns.najibu@gmail.com>
2017-08-19 16:35:29 +0100

    layouts and structure

---
+++ resources/views/posts/show.blade.php
@@ -0,0 +1,5 @@
+@extends ('layout.master')
+
+@section ('content')
+    <h1>A place to show the post.</h1>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,8 @@
 Route::get('/tasks', 'TasksController@index');
 Route::get('/tasks/{task}', 'TasksController@show');
 
+Route::get('/', 'PostsController@index');
+Route::get('/posts/{post}', 'PostsController@show');
 
 Route::get('about', function () {
     return view('about');
