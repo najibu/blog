f39ed2d359f4800b4a7816ae03502105b5a349ff
najibu <ns.najibu@gmail.com>
2017-08-30 13:25:12 +0100

    Tags and pivot tables

--- app/Post.php
+++ app/Post.php
@@ -44,4 +44,11 @@ class Post extends Model
             ->get()
             ->toArray();
     }
+
+    public function tags()
+    {
+        // Any post may have many tags
+        // Any tag may be applied to many posts
+        return $this->belongsToMany(Tag::class);
+    }
 }
